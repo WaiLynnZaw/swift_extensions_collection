4463980c4efe4c3d4de3d91dbf04e9a1244560fc
Wai Lynn Zaw <wailynnzaw@owaytrip.com>
2018-03-28 09:30:45 +0630

    add fade in fade out to view extension

--- UIView.swift
+++ UIView.swift
@@ -75,4 +75,28 @@ public extension UIView {
             print("no action")
         }
     }
+
+
+    /* FADE IN FADE OUT */
+    func fadeIn(duration: TimeInterval = 0.2,
+                delay: TimeInterval = 0.0,
+                completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in }) {
+        UIView.animate(withDuration: duration,
+                       delay: delay,
+                       options: UIViewAnimationOptions.curveEaseIn,
+                       animations: {
+                        self.alpha = 1.0
+        }, completion: completion)
+    }
+    
+    func fadeOut(duration: TimeInterval = 0.2,
+                 delay: TimeInterval = 0.0,
+                 completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in }) {
+        UIView.animate(withDuration: duration,
+                       delay: delay,
+                       options: UIViewAnimationOptions.curveEaseIn,
+                       animations: {
+                        self.alpha = 0.0
+        }, completion: completion)
+    }
 }
